c16620830744b0b21554f14b3669af824e3943cc
jatin-24-wq <jatinvig979@gmail.com>
2024-07-11 04:09:18 +0000

    conversion of datatypre

---
+++ basics/conversionofdatatypes.js
@@ -0,0 +1,5 @@
+let score = "33abc";
+console.log(typeof(score));
+let valueinnumber= Number(score);
+console.log(typeof(valueinnumber));
+console.log(valueinnumber);
--- basics/tst.js
+++ basics/tst.js
@@ -1,1 +1,8 @@
-console.log("jatin")
+console.log("jatin");
+const roomid = ("24")
+let name = "jatin"
+let id = 24
+let money 
+console.table({name ,roomid , id,money})
+let state = null
+console.log(typeof(state))
