3f12c5c03217713d9056683f96500f6085c0da74
jatin-24-wq <jatinvig979@gmail.com>
2024-07-11 04:49:41 +0000

    conversion of operators

--- basics/conversionofdatatypes.js
+++ basics/conversionofdatatypes.js
@@ -1,5 +1,22 @@
-let score = "33abc";
+/*let score = "33abc";
 console.log(typeof(score));
 let valueinnumber= Number(score);
 console.log(typeof(valueinnumber));
-console.log(valueinnumber);
+console.log(valueinnumber);*/
+
+ /*let str1= "jatin"
+ let str2 = "riya"
+ let str3 = str1+str2;
+ 
+ console.log(str3)
+ let str3tonumber = Number(str3)
+ console.log(str3tonumber)
+ console.log(typeof (str3tonumber))
+ console.log( "1" + 2 + 3);
+ console.log(1 + 2 +  "3");*/
+
+ console.log(null>0)
+ console.log(null==0)
+ console.log(null<=0)
+
+console.log("2" === 2) //checks wheather the data type of the conversion is same of not if not thenn it will not convert it and giver you errror//
